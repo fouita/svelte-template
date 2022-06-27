da8b354ae61504af8e987ab7470f2d2cb0eafa75
abdelhay <najjar.abdelhay@gmail.com>
2022-06-27 15:04:29 +0100

    init fouita svelte tailwind template

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "./css/app.css";
+</script>
+
+<div class="p-10 max-w-screen-md mx-auto text-lg">
+  <div class="text-indigo-500 text-2xl text-center p-5">
+    Welcome to Fouita App
+  </div>
+
+  <p>
+    This is an example app that you can update and host to your Fouita project.
+    If you are new to fouita you can get started by building you project.
+  </p>
+</div>
